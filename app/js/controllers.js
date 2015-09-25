a5ccab27a67bc809e8307c66672cef29110742ce
Kiryl Parfiankou <Kiryl.Parfiankou.Dev@gmail.com>
2015-09-26 02:29:06 +0300

    Added angular

---
+++ app/js/controllers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Controllers */
+
+var hummingbirdControllers = angular.module('hummingbirdControllers', []);
+
+hummingbirdControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+hummingbirdControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
